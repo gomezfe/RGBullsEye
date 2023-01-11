79fd4d3d3c588336d760e67ee76f3b329370b514
Felipe <gomezfe@kean.edu>
2023-01-11 14:43:25 -0500

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Felipe Gomez on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
